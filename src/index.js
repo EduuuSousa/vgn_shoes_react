1c51fed5cd426ba9eb0ea5d5644ecaf41f2216d0
EduuuSousa <129524075+EduuuSousa@users.noreply.github.com>
2023-09-07 19:49:42 -0300

    cu

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
 
